39245798370a901aa155c470a770a34fae400a99
Ren <s0985514623@gmail.com>
2024-06-19 15:20:48 +0800

    other main.ts

--- js/src/main.ts
+++ js/src/main.ts
@@ -4,6 +4,7 @@ import '@/assets/scss/index.scss'
   const textarea = $('#r2_wc_line_notify_message')
 
   // 監聽點擊事件
+
   $(document).on('click', '.shortcode-code', function (e) {
     e.preventDefault() // 防止點擊<a>標籤時的預設動作
     const textToInsert = $(this).text() // 獲取<a>標籤的文字
@@ -13,6 +14,7 @@ import '@/assets/scss/index.scss'
     const textAfter = v.substring(cursorPos, v.length) // 當前游標後的文字
 
     // 將文字插入游標位置並更新<textarea>的值
+
     textarea.val(textBefore + textToInsert + textAfter)
   })
 })(jQuery)
